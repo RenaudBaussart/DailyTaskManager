753770f9c6a523230af40fc8252e0df778f5c8a3
Renaud Baussart <renaud.baussart@proton.me>
2025-03-20 11:23:52 +0100

    feat: set up the initialisation of variable for the pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { TaskGroup } from './classes/task-group';
+import { WebStorageService } from './services/web-storage.service';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +12,43 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  // Method to check if an array contains only TaskGroup instances
+  private isTaskGroupArray(arr: any[]): arr is TaskGroup[] {
+    // Use every() to check each item in the array
+    return arr.every(item => item instanceof TaskGroup);
+  }
+
   title = 'DailyTaskManager';
+
+  // Property to store data from local storage
+  taskGroupListStored: any;
+
+  // Property to store task group list
+  taskGroupList: Array<TaskGroup>;
+
+  // Constructor with injected local storage service
+  constructor(public localStoredData: WebStorageService) {
+    
+
+    // Retrieve stored data for "taskList" key
+    this.taskGroupListStored = this.localStoredData.getTaskGroupList("taskList");
+
+    // Check if retrieved data is a valid TaskGroup array
+    if (!Array.isArray(this.taskGroupListStored) || !this.isTaskGroupArray(this.taskGroupListStored)) {
+      // If not valid, initialize taskGroupList as an empty array
+      this.taskGroupList = new Array<TaskGroup>();
+    } else {
+      // If valid, assign retrieved data to taskGroupList
+      this.taskGroupList = this.taskGroupListStored;
+    }
+  }
+  // wait for the page to close
+  @HostListener('window:beforeunload')
+  ngOnDestroy() {
+    // check if there is a taskgroup array in taskGroupList 
+    if(this.isTaskGroupArray(this.taskGroupList)){
+      // save the task group array
+      this.localStoredData.saveTaskGroupList("taskList", this.taskGroupList)
+    }
+  }
 }
