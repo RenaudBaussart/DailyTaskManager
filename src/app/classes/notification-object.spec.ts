9e5f8bae52283f53289451083b202c6a0859695f
Renaud Baussart <renaud.baussart@proton.me>
2025-03-27 11:08:53 +0100

    feat(notification-object): create a class for the notification system

---
+++ src/app/classes/notification-object.spec.ts
@@ -0,0 +1,7 @@
+import { NotificationObject } from './notification-object';
+
+describe('NotificationObject', () => {
+  it('should create an instance', () => {
+    expect(new NotificationObject()).toBeTruthy();
+  });
+});
---
+++ src/app/classes/notification-object.ts
@@ -0,0 +1,12 @@
+export class NotificationObject {
+    public notificationType:string;
+    public message:string;
+
+    constructor(notificationTypeIn:string,messageIn:string){
+            this.notificationType = notificationTypeIn;
+            this.message = messageIn;
+    }
+
+
+
+}
